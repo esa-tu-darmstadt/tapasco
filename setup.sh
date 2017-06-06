d2f237c5b12bc0842d74627c4fa705b95be073d2
Jens Korinth <jk@esa.cs.tu-darmstadt.de>
2017-06-06 13:37:59 +0200

    Allow sourcing of setup.sh from other directories
    * only works for Bash shell; warning for others

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
-export TAPASCO_HOME=$PWD
+if [[ "-bash" == $0 ]]; then
+	export TAPASCO_HOME=`dirname ${BASH_SOURCE[0]} | xargs realpath`
+else
+	echo "WARNING: non-bash shell; need source setup.sh from the TaPaSCo root dir!"
+	export TAPASCO_HOME=$PWD
+fi
+echo "TAPASCO_HOME=$TAPASCO_HOME"
 export PATH=$TAPASCO_HOME/bin:$PATH
 export MANPATH=$MANPATH:$TAPASCO_HOME/man
-# source ~/vivado_15.sh
-sbt assembly
