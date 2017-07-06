37de28facbd917cb56199a4f4abe215882bdf9c6
Jaco Hofmann <hofmann@esa.tu-darmstadt.de>
2017-07-06 11:50:04 +0200

    Adds ZSH to dir-free setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
-if [[ "-bash" == $0 || "bash" == `basename $0` ]]; then
+if [ -n "$BASH_VERSION" ]; then
 	export TAPASCO_HOME=`dirname ${BASH_SOURCE[0]} | xargs realpath`
+elif [ -n "$ZSH_VERSION" ]; then
+	export TAPASCO_HOME=`dirname ${(%):-%x} | xargs realpath`
 else
 	echo "WARNING: non-bash shell; need source setup.sh from the TaPaSCo root dir!"
 	export TAPASCO_HOME=$PWD
