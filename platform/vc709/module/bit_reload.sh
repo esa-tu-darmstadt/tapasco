ed7906b98eba59a432e6e3f911a72a2be9e98ae7
Jens Korinth <jk@esa.cs.tu-darmstadt.de>
2017-07-07 15:27:14 +0200

    Fix typo in bit_reload.sh

--- platform/vc709/module/bit_reload.sh
+++ platform/vc709/module/bit_reload.sh
@@ -43,7 +43,7 @@ hotplug() {
 	VENDOR=10EE
 	DEVICE=7038
 	PCIEDEVICE=`lspci -d $VENDOR:$DEVICE | sed -e "s/ .*//"`
-	echo "hotplugging devive: $PCIEDEVICE"
+	echo "hotplugging device: $PCIEDEVICE"
 	# remove device, if it exists
 	if [ -n "$PCIEDEVICE" ]; then
 		sudo sh -c "echo 1 >/sys/bus/pci/devices/0000:$PCIEDEVICE/remove"
