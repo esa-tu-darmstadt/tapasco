4e2f91de9d9a409a8c94aad57048aeb92b4cd328
Jens Korinth <jk@esa.cs.tu-darmstadt.de>
2017-07-14 18:35:23 +0200

    Update README.md
    * need to rewrite the GETTINGSTARTED documents, removed them for now
    * also deleted 'release.sh' leftover

--- release.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-VERSION=$1
-TEMPBSE=/tmp/tapasco_temp
-TEMPDIR=$TEMPBSE/tapasco/$VERSION
-ZIP=Tapasco-$1.tar.xz
-CURRDIR=`pwd`
-cd $TAPASCO_HOME && cat Release-$1 | xargs tar cvJf $ZIP && pushd /tmp && mkdir -p $TEMPDIR && cd $TEMPDIR && tar xvJf $CURRDIR/$ZIP && cd ../.. && rm $CURRDIR/$ZIP && tar cvJf $CURRDIR/$ZIP tapasco && cd .. && rm -rf $TMPBSE && popd
-
