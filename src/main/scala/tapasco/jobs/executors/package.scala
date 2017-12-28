9538cd680b1efad189fcdc4ab48592d5c0aedcdb
Jens Korinth <jk@esa.cs.tu-darmstadt.de>
2017-12-28 21:05:33 +0100

    Fixes #114 - scalastyle issues
    * fixed new issues; old scalastyle problems persist
    * postponed fixing old issues

--- src/main/scala/tapasco/jobs/executors/package.scala
+++ src/main/scala/tapasco/jobs/executors/package.scala
@@ -4,6 +4,7 @@ import  de.tu_darmstadt.cs.esa.tapasco.task._
 import  de.tu_darmstadt.cs.esa.tapasco.Logging._
 import  de.tu_darmstadt.cs.esa.tapasco.filemgmt.FileAssetManager
 
+// scalastyle:off structural.type
 package object executors {
   trait Executor[-A] { def execute(a: A)(implicit cfg: Configuration, tsk: Tasks): Boolean }
 
@@ -25,23 +26,24 @@ package object executors {
   })
 
   private def checkPlatforms(j: { def platforms: Set[Platform] })(implicit logger: Logger) =
-    if (j.platforms.isEmpty) {
+    j.platforms.nonEmpty || {
       logger.error("no valid Platforms selected! (available: %s)".format(FileAssetManager.entities.platforms map (_.name) mkString ", "))
       false
-    } else true
+    }
 
   private def checkArchs(j: { def architectures: Set[Architecture] })(implicit logger: Logger) =
-    if (j.architectures.isEmpty) {
+    j.architectures.nonEmpty || {
       logger.error("no valid Architectures selected! (available: %s)".format(FileAssetManager.entities.architectures map (_.name) mkString ", "))
       false
-    } else true
+    }
 
   private def checkKernels(j: { def kernels: Set[Kernel] })(implicit logger: Logger) =
-    if (j.kernels.isEmpty) {
+    j.kernels.nonEmpty || {
       logger.error("no valid Kernels selected! (available: %s)".format(FileAssetManager.entities.kernels map (_.name) mkString ", "))
       false
-    } else { logger.info(s"${j.kernels}"); true}
+    }
 
+  // scalastyle:off cyclomatic.complexity
   def check(j: Job)(implicit cfg: Configuration, logger: Logger): Boolean = j match {
     case cj: ComposeJob => checkPlatforms(cj) && checkArchs(cj)
     case cj: CoreStatisticsJob => checkPlatforms(cj) && checkArchs(cj)
@@ -50,4 +52,6 @@ package object executors {
     case ij: ImportJob => checkPlatforms(ij) && checkArchs(ij)
     case _ => true
   }
+  // scalastyle:on cyclomatic.complexity
 }
+// scalastyle:on structural.type
