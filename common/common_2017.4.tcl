3d9121011134c9c6f9b4d51e42dabac57ede5bfa
Jens Korinth <jk@esa.cs.tu-darmstadt.de>
2018-02-08 10:47:15 +0100

    Fix for Vivado 2017.3, 2017.4

---
+++ common/common_2017.4.tcl
@@ -0,0 +1,3 @@
+# create a dictionary of compatible VLNVs
+source $::env(TAPASCO_HOME)/common/common_ip.tcl
+dict set stdcomps   dualdma          vlnv   "esa.informatik.tu-darmstadt.de:user:dual_dma:1.11"
