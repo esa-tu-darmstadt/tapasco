34422ad707ad395983ef585527e6f98e23c0400f
Jens Korinth <jk@esa.cs.tu-darmstadt.de>
2018-03-22 08:29:00 +0100

    tapasco-debug: only show Blue* entries when HW is available
    * implemented check for BlueDMA instances
    * BlueDMA debugging menu entries are only shown when found

--- arch/tests/debug-screens/TapascoDebugMenu.hpp
+++ arch/tests/debug-screens/TapascoDebugMenu.hpp
@@ -24,18 +24,21 @@ using namespace tapasco;
 class TapascoDebugMenu : public MenuScreen {
 public:
   TapascoDebugMenu() : MenuScreen("Welcome to the interactive TaPaSCo Debugger", vector<string>()) {
+    bool blue = has_blue_dma();
     options.push_back("Show kernel map of current bitstream");
     screens.push_back(new TapascoStatusScreen(&tapasco));
     if (tapasco.has_capability(PLATFORM_CAP0_DYNAMIC_ADDRESS_MAP)) {
-      options.push_back("Address map");
+      options.push_back("Show address map of current bitstream");
       screens.push_back(new AddressMapScreen(tapasco));
     }
     options.push_back("Perform interrupt stress test");
     screens.push_back(new InterruptStressTestScreen(&tapasco));
     options.push_back("Monitor device registers");
     screens.push_back(new MonitorScreen(&tapasco));
-    options.push_back("Monitor blue infrastructure");
-    screens.push_back(new BlueDebugScreen(&tapasco));
+    if (blue) {
+      options.push_back("Monitor blue infrastructure");
+      screens.push_back(new BlueDebugScreen(&tapasco));
+    }
     if (tapasco.has_capability(PLATFORM_CAP0_ATSPRI) && tapasco.has_capability(PLATFORM_CAP0_ATSCHECK)) {
       options.push_back("ATS/PRI direct interface");
       screens.push_back(new AtsPriScreen(&tapasco));
@@ -60,7 +63,23 @@ protected:
     }
     return choice;
   }
+
+  bool has_blue_dma() {
+    platform_info_t info;
+    tapasco.info(&info);
+    uint32_t v;
+    for (int c = PLATFORM_COMPONENT_DMA0; c <= PLATFORM_COMPONENT_DMA3; ++c) {
+      if (info.base.platform[c]) {
+        platform_read_ctl(tapasco.platform(), info.base.platform[c] + DMA_ID_REG, sizeof(v), &v, PLATFORM_CTL_FLAGS_NONE);
+	if (v == BLUE_DMA_ID) return true;
+      }
+    }
+    return false;
+  }
+
 private:
+  static constexpr unsigned long DMA_ID_REG = 0x18UL;
+  static constexpr unsigned long BLUE_DMA_ID = 0xE5A0023;
   vector<Screen *> screens;
   Tapasco tapasco;
 };
