b8fce9f18235beba4f6ee5b34e4aedcfefdbb6ef
Jens Korinth <jk@esa.cs.tu-darmstadt.de>
2018-04-09 12:58:10 +0000

    WIP: Zynq DMA management: changed alloc flags
    * changed allocation flags from GFP_DMA (obsolete, discouraged) to
      GFP_KERNEL | __GFP_MEMALLOC
    * with >256MB CMA areas this should mean that we do not run into
      ENOMEMs soon, which could be confirmed
    * however, DMA allocations tend to destabilize the system, sooner
      or later a memory oops will occur (see platform_stress_alloc)
    * not sure how/if to fix that; thought leaving an FPGA-exclusive
      space on top of the DDR via mem= bootarg, but kernel does not
      boot below 500M
    * will keep this for now

--- tlkm/zynq/zynq_dmamgmt.c
+++ tlkm/zynq/zynq_dmamgmt.c
@@ -77,7 +77,7 @@ dma_addr_t zynq_dmamgmt_alloc(size_t const len, handle_t *hid)
 	}
 	_dmabuf.elems[id].kvirt_addr = dma_alloc_coherent(
 			NULL, len, &_dmabuf.elems[id].dma_addr,
-			GFP_DMA | __GFP_RECLAIMABLE);
+			GFP_KERNEL | __GFP_MEMALLOC);
 	if (! _dmabuf.elems[id].kvirt_addr) {
 		WRN("could not allocate DMA buffer of size %zu byte!", len);
 		dmabuf_fsp_put(&_dmabuf, id);
