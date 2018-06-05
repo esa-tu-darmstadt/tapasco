50b91da056e1eb3d00bfec093a35989e56f3ccd9
Jaco Hofmann <hofmann@esa.tu-darmstadt.de>
2018-06-05 16:29:43 +0200

    Fix netfpga sume clock programming example

--- examples/programSI5324/programSI5324.cpp
+++ examples/programSI5324/programSI5324.cpp
@@ -34,7 +34,9 @@
 #include <random>
 
 #include <tapasco.hpp>
-#include <platform.h>
+extern "C" {
+  #include <platform.h>
+}
 #include "pcg-cpp-0.98/include/pcg_random.hpp"
 
 using namespace std;
@@ -86,9 +88,9 @@ typedef struct {
 	bool arb_lost;
 } ISR;
 
-ISR read_isr_register() {
+ISR read_isr_register(Tapasco &tapasco) {
 	uint32_t ret = 0xFFAAFFAA;
-	platform::platform_read_ctl(isr_register, sizeof(ret), &ret, platform::PLATFORM_CTL_FLAGS_RAW);
+	platform_read_ctl(tapasco.platform(), isr_register, sizeof(ret), &ret, PLATFORM_CTL_FLAGS_RAW);
 	ISR t;
 	t.tx_half = getVal(ret, 7);
 	t.not_addressed = getVal(ret, 6);
@@ -101,16 +103,16 @@ ISR read_isr_register() {
 	return t;
 }
 
-void reset_isr_register() {
+void reset_isr_register(Tapasco &tapasco) {
 	uint32_t ret = 0xFFAAFFAA;
-	platform::platform_read_ctl(isr_register, sizeof(ret), &ret, platform::PLATFORM_CTL_FLAGS_RAW);
+	platform_read_ctl(tapasco.platform(), isr_register, sizeof(ret), &ret, PLATFORM_CTL_FLAGS_RAW);
 	ret = 0;
-	platform::platform_write_ctl(isr_register, sizeof(ret), &ret, platform::PLATFORM_CTL_FLAGS_RAW);
+	platform_write_ctl(tapasco.platform(), isr_register, sizeof(ret), &ret, PLATFORM_CTL_FLAGS_RAW);
 }
 
-Status read_status_register() {
+Status read_status_register(Tapasco &tapasco) {
 	uint32_t ret = 0xFFAAFFAA;
-	platform::platform_read_ctl(status_register, sizeof(ret), &ret, platform::PLATFORM_CTL_FLAGS_RAW);
+	platform_read_ctl(tapasco.platform(), status_register, sizeof(ret), &ret, PLATFORM_CTL_FLAGS_RAW);
 	Status t;
 	t.tx_empty = getVal(ret, 7);
 	t.rx_empty = getVal(ret, 6);
@@ -123,78 +125,78 @@ Status read_status_register() {
 	return t;
 }
 
-void enableDevice() {
+void enableDevice(Tapasco &tapasco) {
 	uint32_t ret = 0;
-	platform::platform_read_ctl(control_register, sizeof(ret), &ret, platform::PLATFORM_CTL_FLAGS_RAW);
+	platform_read_ctl(tapasco.platform(), control_register, sizeof(ret), &ret, PLATFORM_CTL_FLAGS_RAW);
 	setVal(ret, 0, 1);
-	platform::platform_write_ctl(control_register, sizeof(ret), &ret, platform::PLATFORM_CTL_FLAGS_RAW);
+	platform_write_ctl(tapasco.platform(), control_register, sizeof(ret), &ret, PLATFORM_CTL_FLAGS_RAW);
 }
 
-void disableDevice() {
+void disableDevice(Tapasco &tapasco) {
 	uint32_t ret = 0;
-	platform::platform_read_ctl(control_register, sizeof(ret), &ret, platform::PLATFORM_CTL_FLAGS_RAW);
+	platform_read_ctl(tapasco.platform(), control_register, sizeof(ret), &ret, PLATFORM_CTL_FLAGS_RAW);
 	setVal(ret, 0, 0);
-	platform::platform_write_ctl(control_register, sizeof(ret), &ret, platform::PLATFORM_CTL_FLAGS_RAW);
+	platform_write_ctl(tapasco.platform(), control_register, sizeof(ret), &ret, PLATFORM_CTL_FLAGS_RAW);
 }
 
-void startTransfer() {
+void startTransfer(Tapasco &tapasco) {
 	uint32_t ret = 0;
-	platform::platform_read_ctl(control_register, sizeof(ret), &ret, platform::PLATFORM_CTL_FLAGS_RAW);
+	platform_read_ctl(tapasco.platform(), control_register, sizeof(ret), &ret, PLATFORM_CTL_FLAGS_RAW);
 	setVal(ret, 2, 1);
-	platform::platform_write_ctl(control_register, sizeof(ret), &ret, platform::PLATFORM_CTL_FLAGS_RAW);
+	platform_write_ctl(tapasco.platform(), control_register, sizeof(ret), &ret, PLATFORM_CTL_FLAGS_RAW);
 }
 
-void endTransfer() {
+void endTransfer(Tapasco &tapasco) {
 	uint32_t ret = 0;
-	platform::platform_read_ctl(control_register, sizeof(ret), &ret, platform::PLATFORM_CTL_FLAGS_RAW);
+	platform_read_ctl(tapasco.platform(), control_register, sizeof(ret), &ret, PLATFORM_CTL_FLAGS_RAW);
 	setVal(ret, 2, 0);
-	platform::platform_write_ctl(control_register, sizeof(ret), &ret, platform::PLATFORM_CTL_FLAGS_RAW);
+	platform_write_ctl(tapasco.platform(), control_register, sizeof(ret), &ret, PLATFORM_CTL_FLAGS_RAW);
 }
 
-void enableGeneralCall() {
+void enableGeneralCall(Tapasco &tapasco) {
 	uint32_t ret = 0;
-	platform::platform_read_ctl(control_register, sizeof(ret), &ret, platform::PLATFORM_CTL_FLAGS_RAW);
+	platform_read_ctl(tapasco.platform(), control_register, sizeof(ret), &ret, PLATFORM_CTL_FLAGS_RAW);
 	setVal(ret, 6, 1);
-	platform::platform_write_ctl(control_register, sizeof(ret), &ret, platform::PLATFORM_CTL_FLAGS_RAW);
+	platform_write_ctl(tapasco.platform(), control_register, sizeof(ret), &ret, PLATFORM_CTL_FLAGS_RAW);
 }
 
-void disableGeneralCall() {
+void disableGeneralCall(Tapasco &tapasco) {
 	uint32_t ret = 0;
-	platform::platform_read_ctl(control_register, sizeof(ret), &ret, platform::PLATFORM_CTL_FLAGS_RAW);
+	platform_read_ctl(tapasco.platform(), control_register, sizeof(ret), &ret, PLATFORM_CTL_FLAGS_RAW);
 	setVal(ret, 6, 0);
-	platform::platform_write_ctl(control_register, sizeof(ret), &ret, platform::PLATFORM_CTL_FLAGS_RAW);
+	platform_write_ctl(tapasco.platform(), control_register, sizeof(ret), &ret, PLATFORM_CTL_FLAGS_RAW);
 }
 
-void resetTXFIFO() {
+void resetTXFIFO(Tapasco &tapasco) {
 	uint32_t ret = 0;
-	platform::platform_read_ctl(control_register, sizeof(ret), &ret, platform::PLATFORM_CTL_FLAGS_RAW);
+	platform_read_ctl(tapasco.platform(), control_register, sizeof(ret), &ret, PLATFORM_CTL_FLAGS_RAW);
 	setVal(ret, 1, 1);
-	platform::platform_write_ctl(control_register, sizeof(ret), &ret, platform::PLATFORM_CTL_FLAGS_RAW);
+	platform_write_ctl(tapasco.platform(), control_register, sizeof(ret), &ret, PLATFORM_CTL_FLAGS_RAW);
 }
 
-void disableTXFIFOReset() {
+void disableTXFIFOReset(Tapasco &tapasco) {
 	uint32_t ret = 0;
-	platform::platform_read_ctl(control_register, sizeof(ret), &ret, platform::PLATFORM_CTL_FLAGS_RAW);
+	platform_read_ctl(tapasco.platform(), control_register, sizeof(ret), &ret, PLATFORM_CTL_FLAGS_RAW);
 	setVal(ret, 1, 0);
-	platform::platform_write_ctl(control_register, sizeof(ret), &ret, platform::PLATFORM_CTL_FLAGS_RAW);
+	platform_write_ctl(tapasco.platform(), control_register, sizeof(ret), &ret, PLATFORM_CTL_FLAGS_RAW);
 }
 
-void setFIFOPIRQ(uint8_t v) {
+void setFIFOPIRQ(Tapasco &tapasco, uint8_t v) {
 	uint32_t ret = v;
-	platform::platform_write_ctl(rx_fifo_pirq, sizeof(ret), &ret, platform::PLATFORM_CTL_FLAGS_RAW);
+	platform_write_ctl(tapasco.platform(), rx_fifo_pirq, sizeof(ret), &ret, PLATFORM_CTL_FLAGS_RAW);
 }
 
-void enqueueWord(uint8_t v, bool start, bool stop) {
+void enqueueWord(Tapasco &tapasco, uint8_t v, bool start, bool stop) {
 	uint32_t f = v;
 	setVal(f, 8, start);
 	setVal(f, 9, stop);
 	std::cout << "Enqueue " << std::hex << f << std::dec << std::endl;
-	platform::platform_write_ctl(fifo_tx_register, sizeof(f), &f, platform::PLATFORM_CTL_FLAGS_RAW);
+	platform_write_ctl(tapasco.platform(), fifo_tx_register, sizeof(f), &f, PLATFORM_CTL_FLAGS_RAW);
 }
 
-uint8_t readWord() {
+uint8_t readWord(Tapasco &tapasco) {
 	uint32_t f = 0xFF;
-	platform::platform_read_ctl(fifo_rx_register, sizeof(f), &f, platform::PLATFORM_CTL_FLAGS_RAW);
+	platform_read_ctl(tapasco.platform(), fifo_rx_register, sizeof(f), &f, PLATFORM_CTL_FLAGS_RAW);
 	return f;
 }
 
@@ -222,43 +224,43 @@ void printISR(ISR t) {
 	std::cout << "Arb Lost: " << 		t.arb_lost << std::endl;
 }
 
-void resetDevice() {
-	platform::platform_write_ctl(reset_register, sizeof(reset_key), &reset_key, platform::PLATFORM_CTL_FLAGS_RAW);
+void resetDevice(Tapasco &tapasco) {
+	platform_write_ctl(tapasco.platform(), reset_register, sizeof(reset_key), &reset_key, PLATFORM_CTL_FLAGS_RAW);
 }
 
-bool writeRegister(uint8_t addr, uint8_t *data, uint16_t words) {
-	resetDevice();
+bool writeRegister(Tapasco &tapasco, uint8_t addr, uint8_t *data, uint16_t words) {
+	resetDevice(tapasco);
 	// Init
-	setFIFOPIRQ(0xF);
-	resetTXFIFO();
-	reset_isr_register();
-	ISR i = read_isr_register();
-	enableDevice();
-	disableTXFIFOReset();
-	disableGeneralCall();
+	setFIFOPIRQ(tapasco, 0xF);
+	resetTXFIFO(tapasco);
+	reset_isr_register(tapasco);
+	ISR i = read_isr_register(tapasco);
+	enableDevice(tapasco);
+	disableTXFIFOReset(tapasco);
+	disableGeneralCall(tapasco);
 
 	std::cout << "Init done" << std::endl;
 
 	// Wait till FIFO is empty
-	Status t = read_status_register();
+	Status t = read_status_register(tapasco);
 	while(!(t.tx_empty && t.rx_empty && !t.bb)) {
 		usleep(1000);
-		t = read_status_register();
+		t = read_status_register(tapasco);
 	}
 	std::cout << "FIFOs empty, ready to go" << std::endl;
 
-	enqueueWord(addr << 1 | 0, true, false);
+	enqueueWord(tapasco, addr << 1 | 0, true, false);
 	for (int i = 0; i < words; ++i) {
-		enqueueWord(data[i], false, i == (words - 1));
+		enqueueWord(tapasco, data[i], false, i == (words - 1));
 	}
 
-	t = read_status_register();
+	t = read_status_register(tapasco);
 	while (!t.tx_empty && t.bb) {
 		usleep(1000);
-		t = read_status_register();
+		t = read_status_register(tapasco);
 	}
-	i = read_isr_register();
-	disableDevice();
+	i = read_isr_register(tapasco);
+	disableDevice(tapasco);
 	if(i.error_compl) {
 		std::cout << "Write unsuccessful." << std::endl;
 		return true;
@@ -268,127 +270,127 @@ bool writeRegister(uint8_t addr, uint8_t *data, uint16_t words) {
 	}
 }
 
-void readRegister(uint8_t addr, uint8_t *data, uint16_t words) {
-	resetDevice();
+void readRegister(Tapasco &tapasco, uint8_t addr, uint8_t *data, uint16_t words) {
+	resetDevice(tapasco);
 	// Init
-	setFIFOPIRQ(0xF);
-	resetTXFIFO();
-	enableDevice();
-	disableTXFIFOReset();
-	disableGeneralCall();
-	reset_isr_register();
+	setFIFOPIRQ(tapasco, 0xF);
+	resetTXFIFO(tapasco);
+	enableDevice(tapasco);
+	disableTXFIFOReset(tapasco);
+	disableGeneralCall(tapasco);
+	reset_isr_register(tapasco);
 
 	std::cout << "Init done" << std::endl;
 
 	// Wait till FIFO is empty
-	Status t = read_status_register();
+	Status t = read_status_register(tapasco);
 	while (!(t.tx_empty && t.rx_empty && !t.bb)) {
 		usleep(1000);
-		t = read_status_register();
+		t = read_status_register(tapasco);
 	}
 	std::cout << "FIFOs empty, ready to go" << std::endl;
 
-	enqueueWord(addr << 1 | 1, true, false);
-	enqueueWord(words, false, true);
+	enqueueWord(tapasco, addr << 1 | 1, true, false);
+	enqueueWord(tapasco, words, false, true);
 
 	std::cout << "Wrote request" << std::endl;
 
 	for (int i = 0; i < words; ++i) {
-		t = read_status_register();
+		t = read_status_register(tapasco);
 		while (t.rx_empty) {
 			usleep(1000);
-			t = read_status_register();
+			t = read_status_register(tapasco);
 		}
-		data[i] = readWord();
+		data[i] = readWord(tapasco);
 	}
 
-	ISR i = read_isr_register();
+	ISR i = read_isr_register(tapasco);
 	printISR(i);
 
-	disableDevice();
+	disableDevice(tapasco);
 }
 
-void readRegisterFull(uint8_t addr, uint8_t slaveReg, uint8_t *data, uint16_t words) {
-	resetDevice();
+void readRegisterFull(Tapasco &tapasco, uint8_t addr, uint8_t slaveReg, uint8_t *data, uint16_t words) {
+	resetDevice(tapasco);
 	// Init
-	setFIFOPIRQ(0xF);
-	resetTXFIFO();
-	enableDevice();
-	disableTXFIFOReset();
-	disableGeneralCall();
+	setFIFOPIRQ(tapasco, 0xF);
+	resetTXFIFO(tapasco);
+	enableDevice(tapasco);
+	disableTXFIFOReset(tapasco);
+	disableGeneralCall(tapasco);
 
 	std::cout << "Init done" << std::endl;
 
 	// Wait till FIFO is empty
-	Status t = read_status_register();
+	Status t = read_status_register(tapasco);
 	while (!(t.tx_empty && t.rx_empty && !t.bb)) {
 		usleep(1000);
-		t = read_status_register();
+		t = read_status_register(tapasco);
 	}
 	std::cout << "FIFOs empty, ready to go" << std::endl;
 
-	enqueueWord(addr << 1 | 0, true, false);
-	enqueueWord(slaveReg, false, false);
-	enqueueWord(addr << 1 | 1, true, false);
-	enqueueWord(words, false, true);
+	enqueueWord(tapasco, addr << 1 | 0, true, false);
+	enqueueWord(tapasco, slaveReg, false, false);
+	enqueueWord(tapasco, addr << 1 | 1, true, false);
+	enqueueWord(tapasco, words, false, true);
 
 	std::cout << "Wrote request" << std::endl;
 
 	for (int i = 0; i < words; ++i) {
-		t = read_status_register();
+		t = read_status_register(tapasco);
 		while (t.rx_empty) {
 			usleep(1000);
-			t = read_status_register();
+			t = read_status_register(tapasco);
 		}
-		data[i] = readWord();
+		data[i] = readWord(tapasco);
 	}
 
-	disableDevice();
+	disableDevice(tapasco);
 }
 
-uint8_t getSwitchPosition() {
+uint8_t getSwitchPosition(Tapasco &tapasco) {
 	uint8_t ret;
-	readRegister(SWITCH_ADDR, &ret, 1);
+	readRegister(tapasco, SWITCH_ADDR, &ret, 1);
 	return ret;
 }
 
-bool setSwitchPosition(uint8_t position) {
-	return writeRegister(SWITCH_ADDR, &position, 1);
+bool setSwitchPosition(Tapasco &tapasco, uint8_t position) {
+	return writeRegister(tapasco, SWITCH_ADDR, &position, 1);
 }
 
-void resetSwitch() {
+void resetSwitch(Tapasco &tapasco) {
 	uint32_t ret = 1;
-	platform::platform_write_ctl(gpo_register, sizeof(ret), &ret, platform::PLATFORM_CTL_FLAGS_RAW);
+	platform_write_ctl(tapasco.platform(), gpo_register, sizeof(ret), &ret, PLATFORM_CTL_FLAGS_RAW);
 	ret = 0;
 	usleep(100000);
-	platform::platform_write_ctl(gpo_register, sizeof(ret), &ret, platform::PLATFORM_CTL_FLAGS_RAW);
+	platform_write_ctl(tapasco.platform(), gpo_register, sizeof(ret), &ret, PLATFORM_CTL_FLAGS_RAW);
 }
 
-void resetClock() {
+void resetClock(Tapasco &tapasco) {
 	uint32_t ret = 2;
-	platform::platform_write_ctl(gpo_register, sizeof(ret), &ret, platform::PLATFORM_CTL_FLAGS_RAW);
+	platform_write_ctl(tapasco.platform(), gpo_register, sizeof(ret), &ret, PLATFORM_CTL_FLAGS_RAW);
 	ret = 0;
 	usleep(1000000);
-	platform::platform_write_ctl(gpo_register, sizeof(ret), &ret, platform::PLATFORM_CTL_FLAGS_RAW);
+	platform_write_ctl(tapasco.platform(), gpo_register, sizeof(ret), &ret, PLATFORM_CTL_FLAGS_RAW);
 }
 
-void releaseResetAll() {
+void releaseResetAll(Tapasco &tapasco) {
 	uint32_t ret = 0x00;
-	platform::platform_write_ctl(gpo_register, sizeof(ret), &ret, platform::PLATFORM_CTL_FLAGS_RAW);
+	platform_write_ctl(tapasco.platform(), gpo_register, sizeof(ret), &ret, PLATFORM_CTL_FLAGS_RAW);
 }
 
 int main(int argc, char **argv) {
 	Tapasco tapasco;
 
-	//resetDevice();
-	resetSwitch();
-	resetClock();
+	//resetDevice(tapasco);
+	resetSwitch(tapasco);
+	resetClock(tapasco);
 
-	if(setSwitchPosition(IIC_BUS_DDR3)) {
+	if(setSwitchPosition(tapasco, IIC_BUS_DDR3)) {
 		std::cout << "Failed to write switch position register." << std::endl;
 		return -1;
 	}
-	std::cout << (uint64_t) getSwitchPosition() << std::endl;
+	std::cout << (uint64_t) getSwitchPosition(tapasco) << std::endl;
 
 	uint8_t WriteBuffer[10];
 	WriteBuffer[0] = 0;
@@ -397,9 +399,9 @@ int main(int argc, char **argv) {
 	WriteBuffer[3] = 0x12;	// Reg 2: BWSEL set to 1
 	WriteBuffer[4] = 0x15;	// Reg 3: CKIN1 selected,  No Digital Hold, Output clocks disabled during ICAL
 	WriteBuffer[5] = 0x92;	// Reg 4: Automatic Revertive, HIST_DEL = 0x12
-	writeRegister(IIC_SI5324_ADDRESS, WriteBuffer, 6);
+	writeRegister(tapasco, IIC_SI5324_ADDRESS, WriteBuffer, 6);
 	uint8_t ReadBuffer[10];
-	readRegisterFull(IIC_SI5324_ADDRESS, 0, ReadBuffer, 5);
+	readRegisterFull(tapasco, IIC_SI5324_ADDRESS, 0, ReadBuffer, 5);
 	for(int i = 0; i < 5; ++i) {
 		if(WriteBuffer[i + 1] != ReadBuffer[i]) {
 			std::cout << "Register " << i << " failed." << std::endl;
@@ -410,45 +412,45 @@ int main(int argc, char **argv) {
 	WriteBuffer[0] = 10;
 	WriteBuffer[1] = 0x08;	// Reg 10: CKOUT2 disabled, CKOUT1 enabled
 	WriteBuffer[2] = 0x40;	// Reg 11: CKIN1, CKIN2 enabled
-	writeRegister(IIC_SI5324_ADDRESS, WriteBuffer, 3);
+	writeRegister(tapasco, IIC_SI5324_ADDRESS, WriteBuffer, 3);
 
 	WriteBuffer[0] = 25;
 	WriteBuffer[1] = 0xA0;
-	writeRegister(IIC_SI5324_ADDRESS, WriteBuffer, 2);
+	writeRegister(tapasco, IIC_SI5324_ADDRESS, WriteBuffer, 2);
 
 	WriteBuffer[0] = 31;
 	WriteBuffer[1] = 0x00;
 	WriteBuffer[2] = 0x00;
 	WriteBuffer[3] = 0x03;
-	writeRegister(IIC_SI5324_ADDRESS, WriteBuffer, 4);
+	writeRegister(tapasco, IIC_SI5324_ADDRESS, WriteBuffer, 4);
 
 	WriteBuffer[0] = 40;
 	WriteBuffer[1] = 0xC2;
 	WriteBuffer[2] = 0x49;
 	WriteBuffer[3] = 0xEF;
-	writeRegister(IIC_SI5324_ADDRESS, WriteBuffer, 4);
+	writeRegister(tapasco, IIC_SI5324_ADDRESS, WriteBuffer, 4);
 
 	WriteBuffer[0] = 43;
 	WriteBuffer[1] = 0x00;
 	WriteBuffer[2] = 0x77;
 	WriteBuffer[3] = 0x0B;
-	writeRegister(IIC_SI5324_ADDRESS, WriteBuffer, 4);
+	writeRegister(tapasco, IIC_SI5324_ADDRESS, WriteBuffer, 4);
 
 	WriteBuffer[0] = 46;
 	WriteBuffer[1] = 0x00;
 	WriteBuffer[2] = 0x77;
 	WriteBuffer[3] = 0x0B;
-	writeRegister(IIC_SI5324_ADDRESS, WriteBuffer, 4);
+	writeRegister(tapasco, IIC_SI5324_ADDRESS, WriteBuffer, 4);
 
 	WriteBuffer[0] = 136;
 	WriteBuffer[1] = 0x40;
-	writeRegister(IIC_SI5324_ADDRESS, WriteBuffer, 2);
+	writeRegister(tapasco, IIC_SI5324_ADDRESS, WriteBuffer, 2);
 
 	ReadBuffer[0] = -1;
-	readRegisterFull(IIC_SI5324_ADDRESS, 136, ReadBuffer, 1);
+	readRegisterFull(tapasco, IIC_SI5324_ADDRESS, 136, ReadBuffer, 1);
 	while(ReadBuffer[0] != 0) {
 		usleep(10000);
-		readRegister(IIC_SI5324_ADDRESS, ReadBuffer, 1);
+		readRegister(tapasco, IIC_SI5324_ADDRESS, ReadBuffer, 1);
 	}
 	return 0;
 }
