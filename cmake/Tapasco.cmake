54a4f413623dea0c1e4823a41dcbde7537569570
Jaco Hofmann <hofmann@esa.tu-darmstadt.de>
2018-07-10 16:55:59 +0200

    Fix policy propagation from include
        - Use proper LTO handling if available

--- cmake/Tapasco.cmake
+++ cmake/Tapasco.cmake
@@ -1,4 +1,6 @@
-cmake_policy(SET CMP0069 NEW)
+if(POLICY CMP0069)
+  cmake_policy(SET CMP0069 NEW)
+endif()
 
 function(set_tapasco_defaults target_name)
     target_compile_options(${target_name} PRIVATE $<$<CXX_COMPILER_ID:GNU>:-Wall>
@@ -8,7 +10,7 @@ function(set_tapasco_defaults target_name)
     set_target_properties(${target_name} PROPERTIES CMAKE_C_STANDARD 11)
 
     if(${CMAKE_VERSION} VERSION_LESS "3.9.0")
-        message("-- Using IPO through fallback! Use Cmake version >= 3.9.0 if possible")
+        message("-- Using IPO through fallback! Use CMake version >= 3.9.0 if possible")
         set_target_properties(${target_name} PROPERTIES CMAKE_AR "gcc-ar")
         set_target_properties(${target_name} PROPERTIES CMAKE_RANLIB  "gcc-ranlib")
         target_compile_options(${target_name} PUBLIC $<$<CXX_COMPILER_ID:GNU>:-flto>
@@ -18,7 +20,6 @@ function(set_tapasco_defaults target_name)
         include(CheckIPOSupported)
         check_ipo_supported(RESULT ipo_supported)
         if(ipo_supported)
-            message("-- IPO is supported!")
             set_target_properties(${target_name} PROPERTIES INTERPROCEDURAL_OPTIMIZATION TRUE)
         else()
             message(WARNING "IPO is not supported!")
