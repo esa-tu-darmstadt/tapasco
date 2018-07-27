bca6143573679c9730d1bcb17cbafec26bb26a6d
Carsten Heinz <heinz@esa.tu-darmstadt.de>
2018-07-27 14:58:58 +0200

    Enable basic support for cross compiling
    - disable examples for now, requires ncurses from other plattform
    - only cmake toolchain supported, tlkm uses makefile

---
+++ runtime/cmake/zynq_cross_toolchain.cmake
@@ -0,0 +1,7 @@
+cmake_minimum_required(VERSION 3.0.0 FATAL_ERROR)
+
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+set(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc)
+set(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++)
