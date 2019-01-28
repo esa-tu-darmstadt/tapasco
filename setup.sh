467180d4a34193acb8b4ac9f73ed8b817f29b6ef
Jaco Hofmann <hofmann@esa.tu-darmstadt.de>
2019-01-28 12:26:58 +0100

    Fix non-greedy match of path in CMake 3.3.2 removal

--- setup.sh
+++ setup.sh
@@ -29,5 +29,6 @@ export TapascoTLKM_DIR=${TAPASCO_HOME}/build/install/usr/local/share/Tapasco/cma
 
 if echo "${PATH}" | grep --quiet "cmake-3.3.2";
 then
-	export PATH=`python -c "import re; print(re.sub(r':.*?cmake-3.3.2.*?:', ':', '${PATH}'));"`
+    echo "Removing old CMake version 3.3.2 distributed with Vivado from Path"
+    export PATH=`python -c "import re; print(re.sub(r'[:][^:]*?cmake[-]3[.]3[.]2.*?[:]', ':', '${PATH}'));"`
 fi
