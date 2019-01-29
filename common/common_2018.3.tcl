c4f747f5746fabcdcd7eb02b75b8f04130c4088c
Jaco Hofmann <hofmann@esa.tu-darmstadt.de>
2019-01-29 18:29:10 +0100

    Add Vivado 2018.3 support

---
+++ common/common_2018.3.tcl
@@ -0,0 +1,6 @@
+# create a dictionary of compatible VLNVs
+source $::env(TAPASCO_HOME)/common/common_ip.tcl
+dict set stdcomps   system_ila       vlnv   "xilinx.com:ip:system_ila:1.1"
+dict set stdcomps   axi_pcie3_0_usp  vlnv   "xilinx.com:ip:xdma:4.1"
+dict set stdcomps   clk_wiz          vlnv   "xilinx.com:ip:clk_wiz:6.0"
+dict set stdcomps   mig_core         vlnv   "xilinx.com:ip:mig_7series:4.1"
