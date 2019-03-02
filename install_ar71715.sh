c65a783dcc392bdd8b868ccb26087907830a4e49
Micha Ober <git@ober-mail.de>
2019-03-02 02:56:17 +0100

    Show command to activate patch if already installed

--- install_ar71715.sh
+++ install_ar71715.sh
@@ -11,21 +11,22 @@ fi
 patchfile="AR71715.zip"
 patchdir="$TAPASCO_HOME/patches/AR71715/"
 
-if [ -d "$patchdir" ]; then
-    echo "Error: $patchdir already exists"
-    exit 1
+if [ ! -d "$patchdir" ]; then
+
+    echo "Download patch..."
+    curl -s https://s3.amazonaws.com/aws-fpga-developer-ami/1.5.0/Patches/$patchfile -o $patchfile
+
+    echo "Extracting patch..."
+    mkdir -p $patchdir
+    unzip $patchfile -d $patchdir
+    rm $patchfile
+
+else
+    echo "Patch already installed."
 fi
-
-echo "Download patch..."
-curl -s https://s3.amazonaws.com/aws-fpga-developer-ami/1.5.0/Patches/$patchfile -o $patchfile
-
-echo "Extracting patch..."
-mkdir -p $patchdir
-unzip $patchfile -d $patchdir
-rm $patchfile
 
 echo "--------------------------------------"
 echo "Activate patch with:"
-cmd="export XILINX_PATH=\"\$TAPASCO_HOME/patches/AR71715/sdx/\""
+cmd="export XILINX_PATH=\"${patchdir}sdx/\""
 echo $cmd
 
