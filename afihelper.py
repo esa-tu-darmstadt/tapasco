9e3fe3f716c4b0bee1a812bb38931557e1eb09da
Micha Ober <git@ober-mail.de>
2019-04-05 01:09:40 +0200

    AFI helper script: Add dry run flag to args

--- afihelper.py
+++ afihelper.py
@@ -23,22 +23,24 @@ import datetime
 
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('bucket',
-	help='name of the target S3 bucket (created when not existing)')
+    help='name of the target S3 bucket (created when not existing)')
 parser.add_argument('tarfile',
-	help='filename of the tar file to create an AFI from')
-parser.add_argument('name',	help='name of the AFI')
+    help='filename of the tar file to create an AFI from')
+parser.add_argument('name', help='name of the AFI')
+parser.add_argument('--dry-run', default=False, action='store_true',
+    help='dry run operation')
 
 args = parser.parse_args()
 
 if not os.path.isfile(args.tarfile):
-	print('ERROR: File does not exist')
-	exit(1)
+    print('ERROR: File does not exist')
+    exit(1)
 
 s3 = boto3.resource('s3')
 
 bucket = s3.Bucket(args.bucket)
 if not bucket.creation_date:
-	s3.create_bucket(Bucket=args.bucket)
+    s3.create_bucket(Bucket=args.bucket)
 
 fname = os.path.basename(args.tarfile)
 s3.meta.client.upload_file(args.tarfile, args.bucket, fname)
@@ -47,7 +49,7 @@ ec2 = boto3.client('ec2')
 
 token = uuid.uuid4().hex
 response = ec2.create_fpga_image(
-    DryRun=True,
+    DryRun=args.dry_run,
     InputStorageLocation={
         'Bucket': args.bucket,
         'Key': fname
@@ -64,4 +66,6 @@ response = ec2.create_fpga_image(
 print(response)
 
 if shutil.which('wait_for_afi.py') is not None:
-	subprocess.call(['wait_for_afi.py', '--afi', response['FpgaImageId']])
+    subprocess.call(['wait_for_afi.py', '--afi', response['FpgaImageId']])
+
+# vim: set expandtab ts=4 sw=4:
