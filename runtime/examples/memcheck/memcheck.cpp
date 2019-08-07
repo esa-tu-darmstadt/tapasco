96ef914505bd1679f562bfb410771d7a54598b45
Jaco Hofmann <hofmann@esa.tu-darmstadt.de>
2019-08-07 13:20:07 +0200

    Remove deprecated copyright header

--- runtime/examples/memcheck/memcheck.cpp
+++ runtime/examples/memcheck/memcheck.cpp
@@ -1,32 +1,6 @@
-//
-// Copyright (C) 2014 Jens Korinth, TU Darmstadt
-//
-// This file is part of Tapasco (TPC).
-//
-// Tapasco is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Tapasco is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with Tapasco.  If not, see <http://www.gnu.org/licenses/>.
-//
-//! @file	memcheck-mt-ff.cc
-//! @brief	Initializes the first TPC device and iterates over a number
-//!  		of integer arrays of increasing size, allocating each array
-//!  		on the device, copying to and from and then checking the
-//!   		results. Basic regression test for platform implementations.
-//!		Single-threaded variant.
-//! @author	J. Korinth, TU Darmstadt (jk@esa.cs.tu-darmstadt.de)
-//!
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 #include <tapasco.hpp>
 
