ce4d7a2220e8141291bfe82b5426992021eb5b4f
Jaco Hofmann <hofmann@esa.tu-darmstadt.de>
2019-11-04 19:47:40 +0100

    Fix path of toolflow for os-package

--- toolflow/os-package/tapasco-init-toolflow.sh
+++ toolflow/os-package/tapasco-init-toolflow.sh
@@ -11,12 +11,12 @@ else
 
     echo "export TAPASCO_HOME=${TAPASCO_HOME}" > tapasco-setup-toolflow.sh
     echo "echo Using TaPaSCo from ${TAPASCO_HOME}" >> tapasco-setup-toolflow.sh
-    echo "export TAPASCO_HOME_TOOLFLOW=${TAPASCO_HOME}/toolflow" >> tapasco-setup-toolflow.sh
+    echo "export TAPASCO_HOME_TOOLFLOW=${TAPASCO_HOME}" >> tapasco-setup-toolflow.sh
     echo "export TAPASCO_HOME_TCL=${TAPASCO_HOME_TOOLFLOW}/vivado" >> tapasco-setup-toolflow.sh
     echo "export TAPASCO_HOME_RUNTIME=${TAPASCO_HOME}/runtime" >> tapasco-setup-toolflow.sh
     echo "export TAPASCO_WORK_DIR=$PWD" >> tapasco-setup-toolflow.sh
 
-    echo "export PATH=\"${TAPASCO_HOME_TOOLFLOW}/bin:${TAPASCO_HOME_RUNTIME}/bin:${TAPASCO_WORK_DIR}/build/install/usr/local/bin/:${TAPASCO_HOME_TOOLFLOW}/scala/build/install/tapasco/bin:\$PATH\"" >> tapasco-setup-toolflow.sh
+    echo "export PATH=\"${TAPASCO_HOME_TOOLFLOW}/bin:\$PATH\"" >> tapasco-setup-toolflow.sh
     echo "export MANPATH=\$MANPATH:$TAPASCO_HOME/man" >> tapasco-setup-toolflow.sh
     echo "export MYVIVADO=\$MYVIVADO:${TAPASCO_HOME_TCL}/common" >> tapasco-setup-toolflow.sh
     echo "export XILINX_PATH=\$XILINX_PATH:${TAPASCO_HOME_TCL}/common" >> tapasco-setup-toolflow.sh
