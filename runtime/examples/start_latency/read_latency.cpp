49c6758b76c5cc048a34f5c2463b8ce96054a5cd
Jaco Hofmann <hofmann@esa.tu-darmstadt.de>
2019-11-18 16:25:21 +0100

    Add measurement for latency of single read/write

---
+++ runtime/examples/start_latency/read_latency.cpp
@@ -0,0 +1,43 @@
+#include <chrono>
+#include <iostream>
+
+#include <platform_types.h>
+#include <tapasco.hpp>
+
+using namespace tapasco;
+
+int main(int argc, char **argv) {
+  Tapasco tapasco;
+
+  constexpr int repetitions = 1000;
+
+  static constexpr tapasco_kernel_id_t COUNTER_ID{14};
+
+  extern volatile void *device_regspace_status_ptr(const platform_devctx_t *devctx);
+
+  uint64_t instances = tapasco_device_kernel_pe_count(tapasco.device(), COUNTER_ID);
+  if (!instances) {
+    std::cout << "Need at least one counter instance to run.";
+    exit(1);
+  }
+
+  volatile void *status = device_regspace_status_ptr(tapasco.platform_device());
+
+  std::chrono::duration<double> elapsed_seconds;
+
+  volatile uint64_t out;
+
+  for (int i = 0; i < repetitions; ++i) {
+    out = ((volatile uint64_t*)status)[0];
+  }
+
+  std::cout << std::fixed << "Single read takes " << elapsed_seconds.count() / repetitions << "s. V: " << out << std::endl;
+
+  for (int i = 0; i < repetitions; ++i) {
+    ((volatile uint64_t*)status)[0] = 42;
+  }
+
+  std::cout << std::fixed << "Single write takes " << elapsed_seconds.count() / repetitions << "s. V: " << out << std::endl;
+
+  return 0;
+}
