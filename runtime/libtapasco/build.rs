55212c42a21623cf771cc0681d16b71c291fb991
Jaco Hofmann <hofmann@esa.tu-darmstadt.de>
2020-05-06 18:27:45 +0200

    Adds initial CMake files for libtapasco

--- runtime/libtapasco/build.rs
+++ runtime/libtapasco/build.rs
@@ -1,17 +1,45 @@
 // build.rs
 extern crate cbindgen;
 
+use cbindgen::Config;
 use std::env;
+use std::path::PathBuf;
 
 fn main() {
     prost_build::compile_protos(&["src/status_core.proto"], &["src/"]).unwrap();
 
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 
+    let package_name = env::var("CARGO_PKG_NAME").unwrap();
+    let output_file = target_dir()
+        .join(format!("{}.h", package_name))
+        .display()
+        .to_string();
+
+    //let config = Config {
+    //    namespace: Some(String::from("tapasco")),
+    //    ..Default::default()
+    //};
+
+    //cbindgen::generate_with_config(&crate_dir, config)
+    //  .unwrap()
+    //  .write_to_file(&output_file);
+
     cbindgen::Builder::new()
-        .with_crate(crate_dir)
+        .with_crate(&crate_dir)
         .with_language(cbindgen::Language::C)
         .generate()
         .expect("Unable to generate bindings")
-        .write_to_file("tapasco.h");
+        .write_to_file(&output_file);
+}
+
+/// Find the location of the `target/` directory. Note that this may be
+/// overridden by `cmake`, so we also need to check the `CARGO_TARGET_DIR`
+/// variable.
+fn target_dir() -> PathBuf {
+    if let Ok(target) = env::var("CARGO_TARGET_DIR") {
+        PathBuf::from(target)
+    } else {
+        PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap()).join("target")
+    }
 }
