aad0b1f11b0e860613f8d7fd3049586d6f65925a
Jaco Hofmann <hofmann@esa.tu-darmstadt.de>
2020-05-07 15:49:41 +0200

    Add copyright headers to all runtime C and C++ files

--- runtime/arch/include/tapasco.hpp
+++ runtime/arch/include/tapasco.hpp
@@ -1,27 +1,29 @@
+/*
+ * Copyright (c) 2014-2020 Embedded Systems and Applications, TU Darmstadt.
+ *
+ * This file is part of TaPaSCo
+ * (see https://github.com/esa-tu-darmstadt/tapasco).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 //! @file   tapasco.hpp
 //! @brief  C++ wrapper class for TAPASCO API: Simplifies calls to
 //!   FPGA and handling of device memory, jobs, etc.
 //! @authors  J. Korinth, TU Darmstadt (jk@esa.cs.tu-darmstadt.de)
 //! @version  1.6
-//! @copyright  Copyright 2015-2018 J. Korinth, TU Darmstadt
-//!
-//!   This file is part of Tapasco (TaPaSCo).
-//!
-//!     Tapasco is free software: you can redistribute it
-//!   and/or modify it under the terms of the GNU Lesser General
-//!   Public License as published by the Free Software Foundation,
-//!   either version 3 of the License, or (at your option) any later
-//!   version.
-//!
-//!     Tapasco is distributed in the hope that it will be
-//!   useful, but WITHOUT ANY WARRANTY; without even the implied
-//!   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-//!   See the GNU Lesser General Public License for more details.
-//!
-//!     You should have received a copy of the GNU Lesser General Public
-//!   License along with Tapasco.  If not, see
-//!   <http://www.gnu.org/licenses/>.
-//!
+
 #ifndef TAPASCO_HPP__
 #define TAPASCO_HPP__
 
--- runtime/platform/common/include/gen_fixed_size_pool.h
+++ runtime/platform/common/include/gen_fixed_size_pool.h
@@ -1,40 +1,24 @@
-//
-// Copyright (C) 2014 Jens Korinth, TU Darmstadt
-//
-// This file is part of Tapasco (TPC).
-//
-// Tapasco is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Tapasco is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with Tapasco.  If not, see <http://www.gnu.org/licenses/>.
-//
 /*
-This file is part of gen_fixed_size_pool.
+ * Copyright (c) 2014-2020 Embedded Systems and Applications, TU Darmstadt.
+ *
+ * This file is part of TaPaSCo
+ * (see https://github.com/esa-tu-darmstadt/tapasco).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
-(C) Copyright 2015 Jens Korinth
-
-gen_fixed_size_pool is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-gen_fixed_size_pool is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with gen_fixed_size_pool.  If not, see <http://www.gnu.org/licenses/>.
 */
-
 //! @file	gen_fixed_size_pool.h
 //! @brief	Generic, header-only, lock-free implementation of a fixed size
 //!		pool of things based on statically allocated array.
