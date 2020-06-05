084efd018d5b16ddd62e2ffb3cfe9299a14c62e2
Marco Hartmann <marco.hartmann@stud.tu-darmstadt.de>
2020-06-05 08:44:08 +0200

    HLS: Add option to kernel.json to give arguments to the kernel testbench

--- toolflow/scala/src/main/scala/tapasco/activity/hls/VivadoHighLevelSynthesis.scala
+++ toolflow/scala/src/main/scala/tapasco/activity/hls/VivadoHighLevelSynthesis.scala
@@ -133,6 +133,7 @@ private object VivadoHighLevelSynthesis extends HighLevelSynthesizer {
     tmpl("SRCSCFLAGS") = k.compilerFlags mkString " "
     tmpl("TBSRCS") = k.testbenchFiles mkString " "
     tmpl("TBCFLAGS") = k.testbenchCompilerFlags mkString " "
+    tmpl("TBARGV") = k.testbenchArgv
     tmpl("DIRECTIVES") = Seq(
       kernelArgs(k, t),
       dirs
