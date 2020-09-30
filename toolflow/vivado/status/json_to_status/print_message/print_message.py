4fef22da768aed01e16149b82e004f1531487a38
Jaco Hofmann <hofmann@esa.tu-darmstadt.de>
2020-09-30 12:45:38 +0200

    Update all the protobuf related files

--- toolflow/vivado/status/json_to_status/print_message/print_message.py
+++ toolflow/vivado/status/json_to_status/print_message/print_message.py
@@ -31,8 +31,8 @@ args = parser.parse_args()
 with open(args.filename, 'rb') as f:
     buf = f.read()
     msg_len, new_pos = _DecodeVarint32(buf, 0)
-    print "Message is {} bytes long.".format(msg_len)
+    print("Message is {} bytes long.".format(msg_len))
     msg = status_core_pb2.Status()
     msg.ParseFromString(buf[new_pos:])
 
-    print msg
+    print(msg)
