411ce43890653d35f250c2b07d8175323a24a642
Torben Kalkhof <kalkhof@esa.tu-darmstadt.de>
2023-07-13 18:14:15 +0200

    Manually connect AI engines
    BD automation fails in some cases, e.g. if a PE has many master interfaces. Hence,
    manually connecting the AI engine is required.

--- toolflow/vivado/platform/versal/plugins/ai_engine.tcl
+++ toolflow/vivado/platform/versal/plugins/ai_engine.tcl
@@ -20,25 +20,43 @@
 if {[tapasco::is_feature_enabled "AI-Engine"]} {
   # add the unconfigured AI engine cell to the block design
   proc create_custom_subsystem_aie {{args {}}} {
+    set aie_clk [create_bd_pin -type "clk" -dir "O" "aie_clk"]
+    set axi_aie [create_bd_intf_pin -mode Slave -vlnv xilinx.com:interface:aximm_rtl:1.0 "S_AIE"]
+
     # name for ai engine needs to be exactly like this for xsa export
     set aie [create_bd_cell -type ip -vlnv xilinx.com:ip:ai_engine:2.0 ai_engine_0]
+
+    connect_bd_net [get_bd_pins $aie/s00_axi_aclk] $aie_clk
+    connect_bd_intf_net $axi_aie [get_bd_intf_pins $aie/S00_AXI]
+
     return $args
   }
 
   namespace eval versal {
-    proc connect_aie_slave {{args {}}} {
-      # connect ai_engine_0/S00_AXI to NoC
-      # do it after general wiring, so that all other ports of the NoC are already connected
-      apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config { Clk_master {/host/versal_cips_0/pmc_axi_noc_axi0_clk (400 MHz)} Clk_slave {/aie/ai_engine_0/s00_axi_aclk (1250 MHz)} Clk_xbar {Auto} Master {/host/versal_cips_0/PMC_NOC_AXI_0} Slave {/aie/ai_engine_0/S00_AXI} ddr_seg {Auto} intc_ip {/memory/axi_noc_0} master_apm {0}}  [get_bd_intf_pins /aie/ai_engine_0/S00_AXI]
+    proc connect_aie_engines {{args {}}} {
+      set old_bd_inst [current_bd_instance .]
+      current_bd_instance "/memory"
 
+      set aie_clk [create_bd_pin -type "clk" -dir "I" "aie_clk"]
+      set axi_aie [create_bd_intf_pin -mode Master -vlnv xilinx.com:interface:aximm_rtl:1.0 "M_AIE"]
+      set cips [get_bd_cells *cips*]
+      set noc [get_bd_cells *noc*]
 
-      # revert some strange changes in simulation model selection (bd automation somehow changes it to tlm)
-      set_property SELECTED_SIM_MODEL rtl [get_bd_cells /memory/axi_noc_0]
+      # create additional clock and AXI master port to NoC and connect AI Engines to PS PMC master
+      set axi_master_num [get_property CONFIG.NUM_MI $noc]
+      set axi_master_name [format "M%02s_AXI" $axi_master_num]
+      set pmc_connections [get_property CONFIG.CONNECTIONS [get_bd_intf_pins $noc/S05_AXI]]
+      set clk_num [get_property CONFIG.NUM_CLKS $noc]
+      set_property CONFIG.NUM_MI [expr $axi_master_num+1] $noc
+      set_property CONFIG.NUM_CLKS [expr $clk_num+1] $noc
+      set_property CONFIG.CATEGORY {aie} [get_bd_intf_pins $noc/$axi_master_name]
+      lappend pmc_connections $axi_master_name {read_bw {100} write_bw {100} read_avg_burst {4} write_avg_burst {4}}
+      set_property CONFIG.CONNECTIONS $pmc_connections [get_bd_intf_pins $noc/S05_AXI]
 
-      # rename sub-block interface names for easier identification for address-map creation
-      set_property name S_AIE [get_bd_intf_pins /aie/S00_AXI]
-      set_property name M_AIE [get_bd_intf_pins /memory/M*_AXI]
+      connect_bd_net $aie_clk [get_bd_pins $noc/aclk$clk_num]
+      connect_bd_intf_net [get_bd_intf_pins $noc/$axi_master_name] $axi_aie
 
+      current_bd_instance $old_bd_inst
       return $args
     }
 
@@ -48,6 +66,6 @@ if {[tapasco::is_feature_enabled "AI-Engine"]} {
     }
   }
 
-  tapasco::register_plugin "platform::versal::connect_aie_slave" "post-wiring"
+  tapasco::register_plugin "platform::versal::connect_aie_engines" "pre-wiring"
   tapasco::register_plugin "platform::versal::aie_addressmap" "post-address-map"
 }
